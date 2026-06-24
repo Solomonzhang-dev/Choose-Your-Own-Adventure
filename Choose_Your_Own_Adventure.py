d2be177ff55aae4fe5a2de7b9da4efb64045fa5f
Solomon Zhang <274030106+Solomonzhang-dev@users.noreply.github.com>
2026-06-24 11:11:38 -0500

    Add files via upload

---
+++ Choose_Your_Own_Adventure.py
@@ -0,0 +1,46 @@
+### Print a welcome message
+print("Welcome to the Haunted Mansion!")
+print("You are a distant family member of a rich millionaire who has just passed away, leaving this mansion to you.")
+print("As the newfound owner, you decide to pay a visit to the mansion.")
+print("The house is dated, creaky, and falling apart. You walk in the front door.")
+print("Do you want to enter the living room or the dining room?")
+
+### Prompt user for a choice
+roomChoice = input("> ")
+
+if(roomChoice == "living room"):
+    print("You enter the living room")
+    print("AS you walk in, you see a sleeping pitbull guarding some gold jewelry.")
+    print("Do you want to steal the jewelry from the pitbull? (yes/no)")
+    
+    pitbullChoice = input("> ")
+    
+    if(pitbullChoice == "yes"):
+        print("You attempt to steal the jewelry, but it wakes up and rips you to shreds.")
+        print("You are now dead...")
+    
+    elif(pitbullChoice == "no"):
+        print("You decide to not steal the dog's jewelry")
+        print("You turn around and leave the house safely")
+    else:
+        print("Invalid choice. Please enter yes or no.")
+    
+elif(roomChoice == "dining room"):
+    print("You chose to go into the dining room.")
+    print("As you walk in, you see a shiny vase on the table.")
+    print("Do you want to open the vase? (yes/no)")
+    
+    vaseChoice = input("> ")
+    
+    if(vaseChoice == "yes"):
+        print("You open the vase and find a pile of bones")
+        print("You fell unconcious afterwards from being grossed out.")
+    elif(vaseChoice == "no"):
+        print("You decide not to open the shiny vase.")
+        print("As you turn to leave, you hear a cracking sound coming from the corner.")
+        print("A dark figure with glowing red eyes launches at you, knocking you unconcious")
+        print("You wake up in your bed. It was all a dream.")
+    else:
+        print("Invalid choice. Please enter yes or no.")
+else:
+    print("Invalid choice. Please enter living room or dining room.")
